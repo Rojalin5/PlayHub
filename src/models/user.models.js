50c79e5fa7104ee7188fc1f0f18d7c48f9da8f20
Rojalin Barik <rojalinbarik2005@gmail.com>
2025-02-12 21:14:24 +0530

    feat: add user channel profile retrieval

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -52,7 +52,6 @@ userSchema.pre("save", async function (next) {
 })
 
 userSchema.methods.isPasswordCorrect = async function(password){
-
     
     return await bcrypt.compare(password, this.password)
 }
