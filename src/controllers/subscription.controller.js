20c68ed9ae3093ec2ba942aeb4c8d3653edc892b
Rojalin Barik <rojalinbarik2005@gmail.com>
2025-03-02 21:24:18 +0530

    feat: add functionality to retrieve user channel subscribers

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,6 +2,8 @@ import { channel } from "diagnostics_channel";
 import { Subscription } from "../models/subscriptions.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { populate } from "dotenv";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const toggelSubscription = asyncHandler(async(req,res)=>{
     const {channelId} = req.params;
@@ -34,4 +36,19 @@ const toggelSubscription = asyncHandler(async(req,res)=>{
     )
 })
 
-export {toggelSubscription}
+const getUserChannelSubscribers = asyncHandler(async(req,res)=>{
+    const {channelId} = req.params;
+    const subscribers = await Subscription.find({channel:channelId})
+    .populate("subscriber","username email fullname avatar")
+    .select("subscriber createdAt")
+
+    if(!subscribers === 0){
+        throw new ApiError(404,"No subscribers found")
+    }
+    return res.status(200).json(
+        new ApiResponse(200,subscribers,"Subscribers fetched Successfully",{
+            TotalSubscribers: subscribers.length
+        })
+    )
+})
+export {toggelSubscription,getUserChannelSubscribers}
