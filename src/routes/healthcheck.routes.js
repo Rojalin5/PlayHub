a350c30d66efc259dd7ef37e160534b041ccf63d
Rojalin Barik <rojalinbarik2005@gmail.com>
2025-03-05 00:00:14 +0530

    feat: add health checkup route

---
+++ src/routes/healthcheck.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/authetication.middlewares.js";
+import { healthCheckup} from "../controllers/healthCheckup.controller.js";
+
+const router = Router()
+
+router.get("/",verifyJWT, healthCheckup)
+export default router
