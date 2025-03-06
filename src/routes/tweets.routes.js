81b101efdcedbfb4c1388d303085cd05ba8c058f
Rojalin Barik <rojalinbarik2005@gmail.com>
2025-03-07 01:08:28 +0530

    feat: add JWT verification to tweet creation route

--- src/routes/tweets.routes.js
+++ src/routes/tweets.routes.js
@@ -4,7 +4,7 @@ import { verifyJWT } from "../middlewares/authetication.middlewares.js";
 
 const router = Router()
 
-router.post("/createtweet",createTweet)
+router.post("/createtweet",verifyJWT,createTweet)
 router.get("/users/:userId",verifyJWT,getUserTweets)
 router.patch("/:tweetId",verifyJWT,updateTweet)
 router.delete("/:tweetId",verifyJWT,deleteTweet)
