b2254854c2f9dc69da8af1b54ceaf9c19ad09f0d
Rojalin Barik <rojalinbarik2005@gmail.com>
2025-03-08 01:39:46 +0530

    feat: update playlist routes

--- src/routes/playlists.routes.js
+++ src/routes/playlists.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/authetication.middlewares.js";
-import { addVideoToPlaylist, createPlaylist, deletePlaylist, getPlaylistById, getUserPlaylists, updatePlaylist } from "../controllers/playlist.controller.js";
+import { addVideoToPlaylist, createPlaylist, deletePlaylist, getPlaylistById, getUserPlaylists, updatePlaylist ,removeVideoFromPlaylist} from "../controllers/playlist.controller.js";
 
 const router = Router()
 
-router.post("/createplaylist", createPlaylist)
+router.post("/createplaylist", verifyJWT,createPlaylist)
 router.get("/users/:userId", verifyJWT, getUserPlaylists)
 router.get("/:playlistId", verifyJWT, getPlaylistById)
+router.delete("/:playlistId/videos/:videoId", verifyJWT, removeVideoFromPlaylist)
 router.post("/:playlistId/videos/:videoId", verifyJWT, addVideoToPlaylist)
 router.delete("/:playlistId", verifyJWT, deletePlaylist)
 router.patch("/:playlistId", verifyJWT, updatePlaylist)
